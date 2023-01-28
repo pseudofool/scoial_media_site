4a6b11889431cfbb39f89182a827a7d5d4eabdb9
pseudofool <saffroncoder@gmail.com>
2023-01-28 17:28:01 +0530

    added routes after sign up

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 main().catch(err =>console.log("Error in connecting to database", err));
 
 async function main(){
-    await mongoose.connect('mongodb://localhost:27017/contacts_list_db');
+    await mongoose.connect('mongodb://localhost/codeial_development');
     console.log("Successfully connected to db!");
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@ module.exports.home = function(req, res){
     // print cookie in console
     console.log(req.cookies);
     // alter the cookie
-    res.cookie('id', 23);
+    // res.cookie('id', 23);
     return res.render('home', {
         title: "Home"
     });
