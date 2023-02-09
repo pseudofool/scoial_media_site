6ce89fae3206d8dfe154db0667807e88753866ea
pseudofool <saffroncoder@gmail.com>
2023-02-09 17:10:18 +0530

    authentication using passport js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// Note that order matters
 const express = require('express');
 const app = express();
 const port = 8000;
 const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts')
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -16,12 +21,27 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes/index'));
-
 // setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// 
+app.use(session({
+    name: 'codeial',
+    // change the secret key before deployment
+    secret: 'somethingblah',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes/index'));
 
 app.listen(port, function(err){
     if(err){
@@ -30,4 +50,4 @@ app.listen(port, function(err){
     }
     console.log(__dirname);
     console.log(`Server is running at port: ${port}`);
-})
+});
